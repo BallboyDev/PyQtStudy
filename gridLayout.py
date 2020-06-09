b1b758b156da73d03a4b43e0283fdfde5e3a5b44
ballboyDev <yswgood0329@naver.com>
2020-06-09 23:41:32 +0900

    layoutStudy

---
+++ gridLayout.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QPushButton, QGridLayout, QApplication)
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        names = ['cls', 'bck', '', 'close',
+                '7', '8', '9', '/',
+                '4', '5', '6', '*',
+                '1', '2', '3', '-',
+                '0', '.', '=', '+']
+
+        position = [(i, j) for i in range(5) for j in range(4)]
+
+        for position, name in zip(position, names):
+            if name == '':
+                continue
+            button = QPushButton(name)
+            grid.addWidget(button, *position)
+
+        self.move(300, 150)
+        self.setWindowTitle('GridLayout')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
